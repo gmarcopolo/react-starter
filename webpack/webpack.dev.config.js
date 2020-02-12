0f55b7159f03f3ced230b9ee7fc748da676b1f34
Marco Polo Guerrero <g.marcopolo@gmail.com>
2020-02-11 19:32:47 -0500

    refact (webpack): update port

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,30 +1,28 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.config.js');
-const webpack = require('webpack');
-const path = require('path');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.config.js");
+const webpack = require("webpack");
+const path = require("path");
 
 const PATH_SRC = path.resolve("src");
 const PATH_DIST = path.resolve("dist");
-const DEV_SERVER_PORT = 8080;
+const DEV_SERVER_PORT = 8082;
 
 module.exports = merge(common, {
-    devtool: "eval-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-
-    ],
-    devServer: {
-        contentBase: PATH_DIST,
-        clientLogLevel: "error",
-        port: DEV_SERVER_PORT,
-        host: "0.0.0.0",
-        open: "true"
-    }
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [],
+  devServer: {
+    contentBase: PATH_DIST,
+    clientLogLevel: "error",
+    port: DEV_SERVER_PORT,
+    host: "0.0.0.0",
+    open: "true"
+  }
 });
